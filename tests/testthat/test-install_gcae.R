839d42dd0adc019e6272b95cb00b62a38096dc57
richelbilderbeek <richel@richelbilderbeek.nl>
2021-07-09 12:45:48 +0200

    Test less on Windows

--- tests/testthat/test-install_gcae.R
+++ tests/testthat/test-install_gcae.R
@@ -1,6 +1,7 @@
 test_that("un- or install in temp gcae folder", {
   expect_equal(1 + 1, 2) # Prevents testthat warning for empty test
   if (!plinkr::is_on_ci()) return()
+  if (plinkr::get_os() == "win") return()
 
   gcae_folder <- normalizePath(get_gcaer_tempfilename(), mustWork = FALSE)
   gcae_options <- create_gcae_options(gcae_folder = gcae_folder)
