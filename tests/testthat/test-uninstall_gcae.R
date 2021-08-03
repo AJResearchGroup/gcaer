bdf446edfd3c3f1a5f3d77eb0f9ec0f7da9d442c
richelbilderbeek <richel@richelbilderbeek.nl>
2021-08-03 13:42:19 +0200

    Codecov

--- tests/testthat/test-uninstall_gcae.R
+++ tests/testthat/test-uninstall_gcae.R
@@ -4,3 +4,12 @@ test_that("use", {
     "must be a list"
   )
 })
+
+test_that("use", {
+  expect_error(
+    uninstall_gcae(
+      gcae_options = create_gcae_options(gcae_folder = "abs.ent")
+    ),
+    "Cannot uninstall a GCAE version that is absent"
+  )
+})
