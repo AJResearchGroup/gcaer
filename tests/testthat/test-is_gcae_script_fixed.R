e9b6f616412ff42dfdeaceab10800684fc3c7a15
richelbilderbeek <richel@richelbilderbeek.nl>
2022-01-12 14:34:52 +0100

    Activate script fixing test

--- tests/testthat/test-is_gcae_script_fixed.R
+++ tests/testthat/test-is_gcae_script_fixed.R
@@ -2,8 +2,7 @@ test_that("use", {
   expect_equal(1 + 1, 2) # Prevents testthat warning for empty test
   if (!plinkr::is_on_ci()) return()
   if (!is_gcae_installed()) return()
-  gcae_options <- create_gcae_options()
-  skip("Not now")
+  gcae_options <- create_gcae_options(gcae_folder = get_gcaer_tempfilename())
   expect_silent(clone_gcae_repo(gcae_options))
   expect_false(is_gcae_script_fixed(gcae_options = gcae_options))
   fix_gcae_script(gcae_options = gcae_options)
