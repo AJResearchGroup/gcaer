a5299ba53f2fa6492ce8659caae253e97347be8e
richelbilderbeek <richel@richelbilderbeek.nl>
2022-02-23 20:45:55 +0100

    Fix test

--- tests/testthat/test-read_labels_file.R
+++ tests/testthat/test-read_labels_file.R
@@ -1,6 +1,5 @@
 test_that("use", {
   labels_filename <- get_gcaer_filename("gcae_input_files_1_labels.csv")
-  t <- read_labels_file(labels_filename)
-  expect_equal(ncol(t), 2)
-  expect_equal(nrow(t), 3)
+  labels_table <- read_labels_file(labels_filename)
+  check_labels_table(labels_table)
 })
