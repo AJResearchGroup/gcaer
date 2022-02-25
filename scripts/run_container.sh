2b8a3b7bdfbb7d0fc4231f5a83248522516932b3
richelbilderbeek <richel@richelbilderbeek.nl>
2022-02-25 10:14:53 +0100

    Build container

---
+++ scripts/run_container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Run the Singularity container on a demo R script
+#
+# Usage
+#
+# ./scripts/run_container.sh
+#
+# --cleanenv: recommened by tsnowlan at https://stackoverflow.com/a/71252619
+# --bind $PWD/scripts/ : bind the folder, so that it works on GitHub Actions as well
+singularity run --cleanenv --bind $PWD/scripts/ gcaer.sif scripts/demo_container.R
+
