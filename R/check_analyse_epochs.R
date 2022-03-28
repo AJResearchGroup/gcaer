3d07ae13edb00c02e2e9f4cb8de0b72b022bf9b8
richelbilderbeek <richel@richelbilderbeek.nl>
2022-03-28 17:30:39 +0200

    EOD

--- R/check_analyse_epochs.R
+++ R/check_analyse_epochs.R
@@ -5,7 +5,9 @@
 #' @inheritParams default_params_doc
 #' @return Nothing. Will \link{stop} if `analyse_epochs` is invalid.
 #' @examples
+#' check_analyse_epochs(1)
 #' check_analyse_epochs(c(1, 2))
+#' check_analyse_epochs(seq(1, 3))
 #' @author Richèl J.C. Bilderbeek
 #' @export
 check_analyse_epochs <- function(
@@ -13,7 +15,9 @@ check_analyse_epochs <- function(
 ) {
   testthat::expect_true(length(analyse_epochs) >= 1)
   testthat::expect_true(is.numeric(analyse_epochs))
-  testthat::expect_true(is.finite(analyse_epochs))
+  testthat::expect_true(all(is.finite(analyse_epochs)))
   testthat::expect_equal(analyse_epochs, round(analyse_epochs))
-  testthat::expect_true(analyse_epochs >= 1)
+  testthat::expect_true(all(analyse_epochs >= 1))
+  testthat::expect_true(all(analyse_epochs == cummax(analyse_epochs)))
+  invisible(analyse_epochs)
 }
--- tests/testthat/test-check_analyse_epochs.R
+++ tests/testthat/test-check_analyse_epochs.R
@@ -1,8 +1,9 @@
 test_that("use", {
-  expect_silent(check_analyse_epochs(analyse_analyse_epochs = 42))
-  expect_silent(check_analyse_epochs(c(1, 2)))
+  expect_silent(check_analyse_epochs(analyse_epochs = 42))
+  expect_silent(check_analyse_epochs(analyse_epochs = c(1, 2)))
   expect_silent(check_analyse_epochs(1))
   expect_error(check_analyse_epochs(0))
+  expect_error(check_analyse_epochs(c(2, 1)))
   expect_error(check_analyse_epochs(-1))
   expect_error(check_analyse_epochs("nonsense"))
   expect_error(check_analyse_epochs(""))
