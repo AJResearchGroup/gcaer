37fa1feaa87f49a7673cc19cbe0087658cab9456
richelbilderbeek <richel@richelbilderbeek.nl>
2022-04-08 13:16:09 +0200

    Improve error

--- R/parse_evaluate_filenames.R
+++ R/parse_evaluate_filenames.R
@@ -15,7 +15,18 @@ parse_evaluate_filenames <- function(
     string = evaluate_filenames,
     pattern = paste0("_pops_epoch_", epoch)
   )
-  testthat::expect_equal(1, length(score_per_pop_filename))
+  if (length(score_per_pop_filename) != 1) {
+    stop(
+      "Must detect exactly one filename containing '_pops_epoch_'. \n",
+      "Number of matches: ", length(score_per_pop_filename), " \n",
+      "Matches: \n * ", paste0(score_per_pop_filename, collapse = "\n * "),
+      " \n",
+      "'evaluate_filenames': ",
+        "\n * ",
+        paste0(evaluate_filenames, collapse = "\n * ")
+    )
+  }
+  testthat::expect_equal(1, )
   testthat::expect_true(file.exists(score_per_pop_filename))
   not_score_per_pop_filenames <- stringr::str_subset(
     evaluate_filenames,
--- tests/testthat/test-parse_evaluate_filenames.R
+++ tests/testthat/test-parse_evaluate_filenames.R
@@ -10,3 +10,17 @@ test_that("use", {
   )
   expect_silent(check_evaluate_results(evaluate_results))
 })
+
+test_that("abuse", {
+  evaluate_filenames <- c(
+    get_gcaer_filename("f1_score_3.csv"),
+    get_gcaer_filename("f1_scores_pops_epoch_1.csv"),
+    get_gcaer_filename("f1_scores_pops_epoch_1.csv")
+  )
+  evaluate_results <- parse_evaluate_filenames(
+    evaluate_filenames = evaluate_filenames,
+    epoch = 1
+  )
+  expect_silent(check_evaluate_results(evaluate_results))
+})
+
