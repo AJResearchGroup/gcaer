b5a2c1cc6d47b5931281badad64e58a932f1c77d
richelbilderbeek <richel@richelbilderbeek.nl>
2022-04-12 10:03:07 +0200

    Put back in tip from Pavlin Mitev

--- scripts/install_gcae_prerequisites.sh
+++ scripts/install_gcae_prerequisites.sh
@@ -9,9 +9,8 @@
 #
 apt-get update && apt-get upgrade -y
 apt-get install -y wget python3-pip grep
-python3 -m pip install --upgrade pip
 # Tip from Pavlin Mitev, instead of 'python3 -m pip install --upgrade pip'
-# python3 -m pip install --no-cache-dir --upgrade pip
+python3 -m pip install --no-cache-dir --upgrade pip
 
 wget https://raw.githubusercontent.com/richelbilderbeek/GenoCAE/Pheno/requirements.txt
 python3 -m pip install -r requirements.txt
