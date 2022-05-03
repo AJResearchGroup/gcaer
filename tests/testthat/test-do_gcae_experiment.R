07159e02f5ca5892f5ee9e297e3599592dad3bbb
richelbilderbeek <richel@richelbilderbeek.nl>
2022-05-03 11:53:07 +0200

    Can use without 'superpops', progress #22

--- tests/testthat/test-do_gcae_experiment.R
+++ tests/testthat/test-do_gcae_experiment.R
@@ -1,5 +1,4 @@
 test_that("use, without labels", {
-  skip("#22")
   expect_equal(1 + 1, 2) # Prevents testthat warning for empty test
   if (!plinkr::is_on_ci()) return()
   if (!is_gcae_script_fixed()) return()
