6da1c44e298c48a464975e06dc05d3d303141fb9
richelbilderbeek <richel@richelbilderbeek.nl>
2022-05-04 13:36:21 +0200

    Doc

--- tests/testthat/test-evaluate_phenotype_prediction.R
+++ tests/testthat/test-evaluate_phenotype_prediction.R
@@ -1,6 +1,9 @@
 test_that("abuse", {
   gcae_experiment_params <- create_test_gcae_experiment_params()
+
+  # Prepare to trigger the error message
   dir.create(gcae_experiment_params$gcae_setup$trainedmodeldir)
+
   expect_error(
     evaluate_phenotype_prediction(
       gcae_experiment_params = gcae_experiment_params,
