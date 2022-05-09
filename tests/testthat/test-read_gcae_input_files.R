0d0edd86d2697cf3f59808879315c81adf8980f5
richelbilderbeek <richel@richelbilderbeek.nl>
2022-05-09 14:12:36 +0200

    Can work with and without labels

--- tests/testthat/test-read_gcae_input_files.R
+++ tests/testthat/test-read_gcae_input_files.R
@@ -3,6 +3,33 @@ test_that("use, with labels", {
   if (!plinkr::is_on_ci()) return()
   if (!plinkr::is_plink_installed()) return()
 
+  gcae_input_filenames <- gcaer::create_test_gcae_input_filenames()
+  expect_true("labels_filename" %in% names(gcae_input_filenames))
+  expect_true(file.exists(gcae_input_filenames$labels_filename))
+  gcae_input_data <- read_gcae_input_files(gcae_input_filenames)
+  expect_true("labels_table" %in% names(gcae_input_data))
+  check_gcae_input_data(gcae_input_data)
+})
+
+test_that("use, without labels", {
+  expect_equal(1 + 1, 2) # Prevents testthat warning for empty test
+  if (!plinkr::is_on_ci()) return()
+  if (!plinkr::is_plink_installed()) return()
+
+  gcae_input_filenames <- gcaer::create_test_gcae_input_filenames()
+  gcae_input_filenames$labels_filename <- NULL
+  expect_false("labels_filename" %in% names(gcae_input_filenames))
+  gcae_input_data <- read_gcae_input_files(gcae_input_filenames)
+  expect_false("labels_table" %in% names(gcae_input_data))
+  check_gcae_input_data(gcae_input_data)
+})
+
+
+test_that("use, with labels", {
+  expect_equal(1 + 1, 2) # Prevents testthat warning for empty test
+  if (!plinkr::is_on_ci()) return()
+  if (!plinkr::is_plink_installed()) return()
+
   gcae_input_filenames <- create_gcae_input_files_1(
     base_input_filename = file.path(
       get_gcaer_tempfilename(),
@@ -11,7 +38,6 @@ test_that("use, with labels", {
   )
   gcae_input_data <- read_gcae_input_files(gcae_input_filenames)
   check_gcae_input_data(gcae_input_data)
-
 
   file.remove(as.character(unlist(gcae_input_filenames)))
 })
