0b3faa29b5362d8c6158d57594296da382c64935
richelbilderbeek <richel@richelbilderbeek.nl>
2022-06-14 18:40:11 +0200

    Fix test

--- tests/testthat/test-get_n_neurons_in_latent_layer.R
+++ tests/testthat/test-get_n_neurons_in_latent_layer.R
@@ -1,4 +1,5 @@
 test_that("M0, from model", {
+  if (!is_gcae_script_fixed()) return()
   model_filename <- get_gcaer_filename("M0.json")
   model <- read_model_file(model_filename)
   n_neurons <- get_n_neurons_in_latent_layer(model)
@@ -6,6 +7,7 @@ test_that("M0, from model", {
 })
 
 test_that("M0, from gcae_experiment_params", {
+  if (!is_gcae_script_fixed()) return()
   gcae_experiment_params <- create_test_gcae_experiment_params()
   expect_equal("M0", gcae_experiment_params$gcae_setup$model_id)
   n_neurons <- get_n_neurons_in_latent_layer(gcae_experiment_params)
