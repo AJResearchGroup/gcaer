4bfbce83b706a485224dfa7bd930d6cc2ddb6b4b
richelbilderbeek <richel@richelbilderbeek.nl>
2022-06-17 11:29:20 +0200

    R-squared works great

--- tests/testthat/test-calc_r_squared.R
+++ tests/testthat/test-calc_r_squared.R
@@ -1,13 +1,24 @@
-test_that("x = y, so r_squared = 1", {
+test_that("worked example 1", {
   # Example from https://www.wallstreetmojo.com/r-squared/
-  n <- 1000
   true_values <- c(5, 10, 4, 1, 3)
   estimated_values <- c(24, 30, 22, 10, 18)
   r_squared <- calc_r_squared(
     true_values = true_values,
     estimated_values = estimated_values
   )
+  # I guess the author made a copy-paste error, as tolerance must be high
   expect_equal(r_squared, 0.8649, tolerance = 0.02)
+})
+
+test_that("worked example 2", {
+  # Example from https://www.delftstack.com/howto/r/calculate-the-r-squared-statistic-in-r/
+  true_values <- c(1, 2, 3, 4, 5)
+  estimated_values <- c(2, 6, 7, 10, 9)
+  r_squared <- calc_r_squared(
+    true_values = true_values,
+    estimated_values = estimated_values
+  )
+  expect_equal(r_squared, 0.8350515, tolerance = 0.0000001)
 })
 
 test_that("no warnings when perfect fit", {
