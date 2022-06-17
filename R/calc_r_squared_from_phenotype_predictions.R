302e3e162eecd3605b56530a82d70970159b47f4
richelbilderbeek <richel@richelbilderbeek.nl>
2022-06-17 11:52:41 +0200

    Fix doc

--- R/calc_r_squared_from_phenotype_predictions.R
+++ R/calc_r_squared_from_phenotype_predictions.R
@@ -2,7 +2,6 @@
 #' from the phenotype predictions
 #'
 #' Calculate the `r_squared` through time
-#' (see \link{calc_r_squared_from_identity_line})
 #' from the phenotype predictions,
 #' @inheritParams default_params_doc
 #' @return a table with the `r_squared` through time
@@ -19,17 +18,6 @@ calc_r_squared_from_phenotype_predictions <- function(phenotype_predictions_tabl
   true_phenotype <- NULL; rm(true_phenotype) # nolint, fixes warning: no visible binding for global variable
   predicted_phenotype <- NULL; rm(predicted_phenotype) # nolint, fixes warning: no visible binding for global variable
 
-  # Same as the version below, yet CRAN package-friendly
-  # (if you know how to do this without changing the user environment,
-  # please let me know)
-  #
-  # phenotype_predictions_table %>%                                             # nolint indeed, this is code
-  #   dplyr::group_by(epoch) %>%                                                # nolint indeed, this is code
-  #   dplyr::summarise(                                                         # nolint indeed, this is code
-  #     r_squared = gcaer::calc_r_squared_from_identity_line(                             # nolint indeed, this is code
-  #       true_phenotype, predicted_phenotype                                   # nolint indeed, this is code
-  #     )                                                                       # nolint indeed, this is code
-  #   )                                                                         # nolint indeed, this is code
   dplyr::summarise(
     dplyr::group_by(
       phenotype_predictions_table,
